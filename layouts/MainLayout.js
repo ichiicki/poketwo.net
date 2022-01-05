19fafaf9d47ff8fca1ff3587b47dab38d3b52c80
ichiicki <84451303+ichiicki@users.noreply.github.com>
2022-01-05 14:12:23 +0530

    Update MainLayout.js

--- layouts/MainLayout.js
+++ layouts/MainLayout.js
@@ -15,7 +15,7 @@ const MainLayout = ({ children }) => {
             >
                 <Navbar.Item href="/">Home</Navbar.Item>
                 {/* <Navbar.Item href="/commands">Commands</Navbar.Item> */}
-                <Navbar.Item href="/people">People</Navbar.Item>
+              
                 <Navbar.Item href="/store">Store</Navbar.Item>
                 <a className="navbar-item" href="https://top.gg/bot/716390085896962058/vote">
                     Vote
